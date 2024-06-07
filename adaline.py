47b93ec1fc910e5a8f1f99ceafc50368452437d6
Jhustyn <jhustyncarvajal@gmail.com>
2024-06-07 08:35:49 -0500

    Para presentar el trabajo

--- adaline.py
+++ adaline.py
@@ -12,9 +12,10 @@ class Adaline:
         self.errores = []
     
     #? Se pude agregar tolerancia_error=1e-5
-    def entrenar(self, X, y, max_iteraciones, estabilizacion_error):
+    def entrenar(self, X, y, max_iteraciones, estabilizacion_error, tolerancia_error=1e-50):
         _, num_caracteristicas = X.shape
         self.pesos = np.random.rand(num_caracteristicas)
+        # self.pesos = np.array([0.5, 0.84, 0.394, 0.783])
         self.errores = []
         
         for iteracion in range(max_iteraciones):
@@ -25,7 +26,7 @@ class Adaline:
                 y_d = np.dot(self.pesos, muestra) + self.sesgo
                 error = y[indice_X] - y_d
                 self.pesos += self.factor_aprendizaje * error * muestra
-                error_total_iteracion += error
+                error_total_iteracion += error**2
 
                 print(f'\n--- Iteración {iteracion + 1} -> Patrón {muestra}: {y[indice_X]} ---')
                 print(f'Valor de y: {round(y_d, 4)}')
@@ -33,6 +34,7 @@ class Adaline:
                 print(f'Pesos: {self.pesos}')
                 print(f'Error total: {round(error_total_iteracion, 4)}')
 
+            # self.errores.append(np.abs(error_total_iteracion))
             self.errores.append(error_total_iteracion)
 
             if (len(self.errores) > estabilizacion_error and 
@@ -40,14 +42,15 @@ class Adaline:
                 print(f"\n!! Deteniendo temprano ya que el error no ha cambiado en las últimas {estabilizacion_error} iteraciones.")
                 break
             
-            # if abs(error_total_iteracion) < tolerancia_error:
-            #     print(f"\n!! Deteniendo temprano ya que el error total ({round(error_total_iteracion, 4)}) es menor que la tolerancia de error ({tolerancia_error}).")
-            #     break
+            if abs(error_total_iteracion) < tolerancia_error:
+                print(f"\n!! Deteniendo temprano ya que el error total ({round(error_total_iteracion, 4)}) es menor que la tolerancia de error ({tolerancia_error}).")
+                break
 
             if iteracion + 1 >= max_iteraciones:
                 print('\nMáximo de iteraciones alcanzado')
                 break
         
+        # self.errores =  np.square(self.errores)
         self.error_total_modelo = sum(self.errores)
         self.guardar_pesos()
     
@@ -88,6 +91,16 @@ class Adaline:
         except Exception as e:
             print(e)
 
+def vector_a_matriz(vector):
+    return [vector[i:i+5] for i in range(0, len(vector), 5)]
+
+# Función para imprimir una matriz 5x5 en la consola
+def imprimir_matriz(matriz):
+    print('\n')
+    for fila in matriz:
+        print(" ".join(str(celda) for celda in fila))
+
+
 def load_dataset(csv_path):
     data = pd.read_csv(csv_path)
     X = data.iloc[:, :-1].values
@@ -97,7 +110,7 @@ def load_dataset(csv_path):
 def main(): 
     
     op = 0
-    adaline = Adaline(0, 0.3)
+    adaline = Adaline(0, 0.1)
     while op!=4:
         print('\n------Adeline------')
         print('1) Entrenar red')
@@ -115,11 +128,15 @@ def main():
                     csv_path = input("Ingrese el path del dataset CSV: ") 
                     csv_path = f'CSV/{csv_path}'
                     X, y = load_dataset(csv_path)
- 
-                    adaline.entrenar(X, y, max_iteraciones=1000, estabilizacion_error=50)
+
+                    adaline.entrenar(X, y, max_iteraciones=100, estabilizacion_error=5)
 
                     print("\nEntrenamiento completado.")
-                    # print(perceptron.errores)
+                    
+                    print('-----IMPRIMIR LA ENTRADA-----')
+                    n,m = X.shape
+                    [imprimir_matriz(vector_a_matriz(X[n])) for n  in range(n)]
+                    
                     print("\n---Grafica de errores---")
                     adaline.graficar_errores()
                     
@@ -128,13 +145,22 @@ def main():
             case 2:   
                 try:  
                     print('\n---Clasificar muestras---')
+                    
                     csv_path = input("Ingrese el path del dataset CSV: ") 
                     csv_path = f'CSV/{csv_path}'
-                    X, y = load_dataset(csv_path)
+                    X_test, y_test = load_dataset(csv_path)
                     
-                    for indice, muestra in enumerate(X):
-                        clasificacion = adaline.predecir(muestra)
-                        print(f'Muestra: {muestra} --> Valor Esperado:{y[indice]} -> Clasificacion: {round(clasificacion,2)}')
+                    predictions = [adaline.predecir(muestra) for muestra in X_test]
+
+                    # Evaluate the predictions
+                    print("Predicciones vs Valores Reales:")
+                    for i, (pred, real) in enumerate(zip(predictions, y_test)):
+                        print(f"Muestra {i+1}: Predicción = {round(pred, 2)}, Valor Real = {real}")
+
+                    print('-----IMPRIMIR LA ENTRADA-----')
+                    n,m = X_test.shape
+                    [imprimir_matriz(vector_a_matriz(X_test[n])) for n  in range(n)]
+                
                 except Exception as e:
                     print(e)
             case 3:
